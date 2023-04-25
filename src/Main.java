9372ca990d1073fac685ba613a235c13bac17e57
zaczytana <zaczytanazuzana@gmail.com>
2023-04-25 17:50:36 +0200

    added sout with test2222

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("test123");
         System.out.println("test2222");
+        System.out.println("zmiany na developie");
     }
 }
